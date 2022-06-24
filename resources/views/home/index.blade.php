cf798677d18179b0d8a2ce5abdafe761ff281b6b
shinopai <harbor.shinozaki@gmail.com>
2022-06-24 07:39:52 +0000

    breeze install

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+  <!-- header -->
+  @include('layouts.header')
+</x-app-layout>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <nav class="header__nav wrap">
+    <div class="header__inner flex">
+      <div class="header__flex--item">地元の掲示板 ジモティー</div>
+      <div class="header__flex--item">
+        flex
+      </div>
+    </div>
+  </nav>
+</header>
